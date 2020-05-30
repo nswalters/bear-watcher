d44a6fab2a56e8839aa01b7939407b7ea79e715d
Nick Walters <nswalters.s@gmail.com>
2020-05-30 12:21:30 -0500

    Add bear data module

---
+++ js/helpers/data/bearData.js
@@ -0,0 +1,20 @@
+let bears = [
+  // {
+  //   name: 'Cinnamon',
+  //   imageURL: 'https://images.unsplash.com/flagged/photo-1577313684536-9f5124c1adc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+  // },
+  // {
+  //   name: 'Charlie',
+  //   imageURL: 'https://images.unsplash.com/photo-1568162603664-fcd658421851?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1273&q=80'
+  // },
+  // {
+  //   name: 'Goldilocks',
+  //   imageURL: 'https://images.unsplash.com/photo-1545319832-d04e0052479f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80'
+  // },
+];
+
+const getBears = () => bears;
+
+export default {
+  getBears
+};
