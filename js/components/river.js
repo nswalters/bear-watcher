77bf9188d5e23c04c400faf1e1b7e30bdc57a812
Nick Walters <nswalters.s@gmail.com>
2020-05-30 21:30:38 -0500

    Added updated styling for the bear cards

--- js/components/river.js
+++ js/components/river.js
@@ -8,11 +8,26 @@ const makeRiver = () => {
 
   bears.forEach(bear => {
     domString += `
-    <div class="card">
-      <img src="${bear.imageURL}" alt="Image of Tracked Bear" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">${bear.name}</h5>
-        <p class="card-txt"></p>
+    <div class="card bear-card">
+      <img src="${bear.imageURL}" alt="Image of Tracked Bear" class="card-img-top card-image">
+      <div class="bear-name-holder d-flex justify-content-center align-items-center px-2">
+        ${bear.name}
+      </div>
+      <div class="card-body d-flex p-0 bear-card-body">
+        <div class="card-body-left col col-6">
+          <div class="bear-icons d-flex justify-content-between">
+            <i class="far fa-plus-square fa-2x bear-success-icon"></i>
+            <span class="bear-success-icon bear-success-count">110</span>
+            <i class="fas fa-fish fa-2x bear-success-icon right-5"></i>
+          </div>
+        </div>
+        <div class="card-body-right col col-6">
+          <div class="bear-icons d-flex flex-row-reverse justify-content-between">
+            <i class="far fa-minus-square fa-2x bear-failed-icon"></i>
+            <span class="bear-failed-icon bear-fail-count">54</span>
+            <i class="fas fa-fish fa-2x bear-failed-icon fa-flip-horizontal"></i>
+          </div>
+        </div>
       </div>
     </div>
     `
