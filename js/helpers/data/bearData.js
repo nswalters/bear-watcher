0507b6b836c6a4e1bbb7dcc71340ef305ca7bd5d
Nick Walters <nswalters.s@gmail.com>
2020-05-31 16:41:34 -0500

    Added unique bear ids, caught, and missed counters per bear

--- js/helpers/data/bearData.js
+++ js/helpers/data/bearData.js
@@ -4,8 +4,11 @@ const getBears = () => bears;
 
 const setBears = (bearName, bearImageURL) => {
   const bear = {
+    id: Date.now(),
     name: bearName,
-    imageURL: bearImageURL
+    imageURL: bearImageURL,
+    caughtCount: 0,
+    missedCount: 0,
   }
 
   bears.push(bear);
