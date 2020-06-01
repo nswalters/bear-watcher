dae4a2bcff8a3249a0015c089f0d159ee5c62c07
Nick Walters <nswalters.s@gmail.com>
2020-05-31 20:49:09 -0500

    Added timestamp function to manage timestamp data on cards

--- js/components/river.js
+++ js/components/river.js
@@ -13,6 +13,7 @@ const addCaughtFish = (e) => {
   bears.forEach(bear => {
     if (bear.id == e.target.id) {
       bear.caughtCount += 1;
+      (bear.caughtTimestamps).push(Date.now());
     }
   });
 
@@ -26,10 +27,52 @@ const addMissedFish = (e) => {
   bears.forEach(bear => {
     if (bear.id == e.target.id) {
       bear.missedCount += 1;
+      (bear.missedTimestamps).push(Date.now());
     }
   });
 
   makeRiver();
+}
+
+const addTimestampData = (timestamps) => {
+
+  let domString = '';
+
+  for (let index = 0; index < timestamps.length; index++) {
+
+    // Do some timestamp mangling to convert to human-readable timestamp
+    const timestamp = new Date(timestamps[index]);
+
+    const convertedTimestamp = timestamp.getFullYear() + '-' +
+      ('0' + (timestamp.getMonth() + 1)).slice(-2) + '-' +
+      ('0' + timestamp.getDate()).slice(-2) + ' ' +
+      timestamp.getHours() + ':' +
+      ('0' + timestamp.getMinutes()).slice(-2);
+
+    // Start 'row' div every two timestamps
+    if (index % 2 === 0) {
+      domString += `<div class="row">`;
+    }
+
+    // Add timestamp content to row
+    domString += `
+    <div class="col col-6 px-0">
+      <div>${convertedTimestamp}</div>
+    </div>
+    `
+
+    // End 'row' div after every second element (every index that's odd)
+    if (index % 2 !== 0) {
+      domString += `</div>`
+    }
+  };
+
+  // Handles adding the closing row div if an odd number of timestamps are present
+  if (timestamps.length % 2 !== 0) {
+    domString += `</div>`;
+  }
+
+  return domString;
 }
 
 
@@ -52,151 +95,29 @@ const makeRiver = () => {
             <span class="bear-success-icon bear-success-count">${bear.caughtCount}</span>
             <i class="fas fa-fish fa-2x bear-success-icon right-5"></i>
           </div>
-          <div class="timestamp-container container">
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-              <div class="col col-6">
-                <div>2020-05-30 21:41</div>
-              </div>
-            </div>
-          </div>
-        </div>
+          <div class="timestamp-container container">`
+    domString += addTimestampData(bear['caughtTimestamps']);
+
+    domString += `</div>` // closes timestamp-container
+    domString += `</div>` // closes card-body-left
+
+    domString += `
         <div class="card-body-right col col-6">
           <div class="bear-icons d-flex flex-row-reverse justify-content-between">
             <i class="far fa-minus-square fa-2x bear-failed-icon" id="${bear.id}"></i>
             <span class="bear-failed-icon bear-fail-count">${bear.missedCount}</span>
             <i class="fas fa-fish fa-2x bear-failed-icon fa-flip-horizontal"></i>
           </div>
-          <div class="timestamp-container container">
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-                <div class="col col-6">
-                  <div>2020-05-30 21:41</div>
-                </div>
-            </div>
-            <div class="row">
-              <div class="col col-6">
-                <div>2020-05-30 21:41</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    `
+          <div class="timestamp-container container">`
+
+    domString += addTimestampData(bear['missedTimestamps']);
+
+    domString += `</div>` // closes timestamp-container
+    domString += `</div>` // closes card-body-right
+
+    domString += `</div>` // closes card-body
+    domString += `</div>` // closes card
+
   });
 
   utils.printToDom('#bear-collection', domString);
